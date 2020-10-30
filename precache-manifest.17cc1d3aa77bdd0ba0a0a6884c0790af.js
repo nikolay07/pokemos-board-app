dde5b0e8e768795f95167d617f00ccae8bad33c6
nikolay07 <nikolay0701@ukr.net>
2020-10-30 13:34:22 +0200

    Updates

---
+++ precache-manifest.17cc1d3aa77bdd0ba0a0a6884c0790af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed55defb558073e3b07ce30e73f86731",
+    "url": "/pokemos-board-app/index.html"
+  },
+  {
+    "revision": "ee8411f2f2f055c0b0f2",
+    "url": "/pokemos-board-app/static/css/main.6c05391d.chunk.css"
+  },
+  {
+    "revision": "3c9732cfd0be810feebb",
+    "url": "/pokemos-board-app/static/js/2.de00b5c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pokemos-board-app/static/js/2.de00b5c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8411f2f2f055c0b0f2",
+    "url": "/pokemos-board-app/static/js/main.0a6d2384.chunk.js"
+  },
+  {
+    "revision": "a877b7fdd650514cdba3",
+    "url": "/pokemos-board-app/static/js/runtime-main.1e6a905a.js"
+  }
+]);
